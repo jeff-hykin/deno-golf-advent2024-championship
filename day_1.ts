a5f1d12ea14977517f331ff0829c6cdb0a8009af
Chris Cowan <agentme49@gmail.com>
2024-12-01 20:22:19 -0800

    day 1 part 1

--- day_1.ts
+++ day_1.ts
@@ -1,13 +1,20 @@
 import { assertEquals } from "@std/assert";
+import { zip } from "@std/collections/zip";
 import { runPart } from "@macil/aocd";
 
-function parse(input: string) {
-  return input.trimEnd().split("\n").map(Number);
+function parse(input: string): Array<[number, number]> {
+  return input.trimEnd().split("\n").map((line) => {
+    const [a, b] = line.split(/\s+/).map(Number);
+    return [a, b];
+  });
 }
 
 function part1(input: string): number {
   const items = parse(input);
-  throw new Error("TODO");
+  const l1 = items.map((pair) => pair[0]).sort((a, b) => a - b);
+  const l2 = items.map((pair) => pair[1]).sort((a, b) => a - b);
+  const pairs = zip(l1, l2);
+  return pairs.map(([a, b]) => Math.abs(a - b)).reduce((a, b) => a + b, 0);
 }
 
 // function part2(input: string): number {
@@ -21,11 +28,12 @@ if (import.meta.main) {
 }
 
 const TEST_INPUT = `\
-6
-7
-8
-9
-10
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 `;
 
 Deno.test("part1", () => {
