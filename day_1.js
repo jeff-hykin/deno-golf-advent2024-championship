0de58a1a51ff542586811c9a868e0589786a8152
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-03 03:05:02 -0600

    optimal

--- day_1.js
+++ day_1.js
@@ -1,4 +1,1 @@
-var p=await new Response(Deno.stdin.readable).text(),c=0,s=0,i=p.split(/\s/g).map((e,j)=>(p=j)&1?-e:e).sort()
-i.slice(++p/2).map((e,j,k)=>{s+=Math.abs((j=-i[j])-e)
-k.map(h=>h==j&&(c+=j))})
-console.log(s,c)
+new Response(Deno.stdin).text().then((p,c=0,s=0,i=p.split(/\s/g).map((e,j)=>(p=j)&1?-e:e).sort())=>i.slice(++p/2).map((e,j,k)=>s+=Math.abs((j=-i[j])-e)+!k.map(h=>c+=j*(h==j)))+console.log(s,c))
